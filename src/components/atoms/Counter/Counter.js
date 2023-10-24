193013dd67adcfb46b95d4765bd27e34122b19df
Makatka <natalii.mazur@gmail.com>
2023-10-24 11:42:47 +0200

    Add countdown logic

--- src/components/atoms/Counter/Counter.js
+++ src/components/atoms/Counter/Counter.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import {CounterStyles} from "./Counter.styles";
 
 
-const Counter = () => {
+const Counter = ({durationDays}) => {
+  const [time, setTime] = useState(durationDays * 24 * 60 * 60);
+
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTime(time - 1);
+    }, 1000)
+    return () => clearTimeout(timer);
+  }, [time])
+
+
   return (
     <CounterStyles>
-      <div>00</div>
-      <span>-</span>
-      <div>13</div>
-      <span>-</span>
-      <div>57</div>
-      <span>-</span>
-      <div>42</div>
+      <div>{("0" + Math.floor(time / 86400)).slice(-2)}</div>
+      <span> - </span>
+      <div>{("0" + Math.floor((time / 3600) % 24)).slice(-2)}</div>
+      <span> - </span>
+      <div>{("0" + Math.floor((time / 60) % 60)).slice(-2)}</div>
+      <span> - </span>
+      <div>{("0" + Math.floor(time % 60)).slice(-2)}</div>
     </CounterStyles>
   );
 };
--- src/components/molecules/CounterBanner/CounterBanner.js
+++ src/components/molecules/CounterBanner/CounterBanner.js
@@ -10,7 +10,7 @@ const CounterBanner = () => {
         <SmallLabel text = 'Deals fo the week'/>
         <h2>Reflect the bonds of the past into your <span>modern</span> life.</h2>
         <p>Fashion is about dressing according to what's fashionable. Style is more about being yourself. Design is a constant challenge to balance comfort with luxe...</p>
-        <Counter/>
+        <Counter durationDays = {35}/>
         <button>Buy Products</button>
       </BannerText>
     </Wrapper>
